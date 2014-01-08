6c7ff4a0a16976a400cf2cee8e4b4a4222c26137
Pete Lorenzo <lorenzo@poixson.com>
2014-01-08 14:31:30 -0500

    cleaned up display.php a bit, and fixed a small spacing glitch

--- www/WebInterface/html/default/display.php
+++ www/WebInterface/html/default/display.php
@@ -7,45 +7,38 @@ $outputs=array();
 $outputs['item']='
 <div style="padding-top: 10px; padding-bottom: 10px; text-align: center;">
 <img src="{item image url}" alt="{item title}" style="width: 32px; height: 32px; margin-bottom: 5px;" /><br />
+
 <b>{item name}</b>
-{has damage}
-  <br />{item damage}
-{/has damage}
-{has custom name}
-  <br />{custom name}
-{/has custom name}
-{has lore}
-  <br />{lore}
-{/has lore}
-{has enchantments}
-  <br />{enchantments}
-{/has enchantments}
+{has damage}<br />{item damage}{/has damage}
+{has custom name}<br />{custom name}{/has custom name}
+{has lore}<br />{lore}{/has lore}
+{has enchantments}<br />{enchantments}{/has enchantments}
+
+<br />
 </div>
 ';
 
 
 // custom name
-$outputs['custom name']='
-<font size="-1" class="display_custom_name"><b>{name}</b></font>
+$outputs['custom name']=
+'<font size="-1" class="display_custom_name"><b>{name}</b></font>
 ';
 
 
 // lore
-$outputs['lore line']='
-<font size="-1" class="display_lore"><i>{line}</i></font>
+$outputs['lore line']=
+'<font size="-1" class="display_lore"><i>{line}</i></font>
 ';
-$outputs['lore split']='
-<br />
-';
+$outputs['lore split']=
+'<br />';
 
 
 // enchantment
-$outputs['enchantment']='
-<font size="-1">{ench title} {ench level}</font>
+$outputs['enchantment']=
+'<font size="-1">{ench title} {ench level}</font>
 ';
-$outputs['enchantment split']='
-<br />
-';
+$outputs['enchantment split']=
+'<br />';
 
 
 return($outputs);
