34a30e51b4f6dd9f8909ad8a13cb069bf6514ebf
lorenzop <lop@matt.lophome.com>
2014-12-07 00:41:34 -0500

    changed version from 1.1.15 to 1.2.x also set author MarioG1

--- www/WebInterface/index.php
+++ www/WebInterface/index.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
 define('DEFINE_INDEX_FILE',TRUE);
-define('CURRENT_VERSION', '1.1.15');
+define('CURRENT_VERSION', '1.2.x-SNAPSHOT');
 global $config;
 
 // get,post,cookie (highest priority last)
